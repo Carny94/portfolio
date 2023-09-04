a931afa040593b312f79d81f0bdcf9410fed66c0
mookdook94 <134980150+mookdook94@users.noreply.github.com>
2023-09-03 18:16:40 -0700

    created experience table

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,60 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default function App() {
+  return (
+    <main className="App">
+      
+      <h2>Hi There Im</h2>
+     <h1>Carneala Odom</h1> 
+
+     <p class="About-me">
+      I am a Bay Area Native full-stack developer who takes joy in learning new coding languages❤️
+      Transitioning from a career in property management, I've embarked on an exciting journey into the dynamic 
+      world of web development, and I'm here to make a statement. My love for learning is the driving force behind my career transition.
+      I thrive on the challenges that coding languages present and embrace every opportunity to expand my skill set. 
+      In this ever-evolving field, I see each project as an opportunity to grow, adapt, and push the boundaries of what's possible.
+     </p>
+
+  <div className="Experience-container">
+  
+  <div className="frontend-table">
+    <h2>Front End</h2>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>Bootstrap</li>
+      <li>JavaScript</li>
+      <li>React</li>
+      <li>JQuery</li>
+    </ul>
+  </div>
+
+  <div className="backend-table">
+  <h2>Back End</h2>
+  <ul>
+    <li>Node.js</li>
+    <li>Express.js</li>
+    <li>PostgreSQL</li>
+    <li>MongoDB</li>
+    <li>Python</li>
+    <li>Django</li>
+  </ul>
+</div>
+</div>
+
+
+
+     
+
+      
+
+
+      
+
+    </main>
+  );
+}
+
+

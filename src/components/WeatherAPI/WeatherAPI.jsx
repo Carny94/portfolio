9537dd84349bd5601d64d737159a397cc07f4865
mookdook94 <134980150+mookdook94@users.noreply.github.com>
2023-10-10 15:59:53 -0700

    working on api in frontend spec. working on what will be returned

--- src/components/WeatherAPI/WeatherAPI.jsx
+++ src/components/WeatherAPI/WeatherAPI.jsx
@@ -3,9 +3,42 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
-export default function WeatherAPI () {
+export default function WeatherAPI() {
+
+    const apiUrl = 'https://us-weather-by-zip-code.p.rapidapi.com/getweatherzipcode';
+    const [weatherData, setWeatherData] = useState([]);
+    const [loading, setLoading] = useState(true);
 
 
+    async function fetchWeatherAPI() {
+
+        try {
+
+            const response = await axios.get(apiUrl);
+            setWeatherData(response.data);
+            setLoading(false);
+
+        } catch (error) {
+
+            console.error(error);
+            setLoading(false);
+        }
+
+
+    }
+
+
+    return (
+
+        <div className='.rectangle-container' >
+            
+        </div>
+
+
+
+
+
+    )
 
 }
 

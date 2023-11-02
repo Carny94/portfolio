d9b3b27bc0023d68bc8247c40a35f4d5e65edf1d
mookdook94 <134980150+mookdook94@users.noreply.github.com>
2023-11-02 16:54:10 -0700

    added media query . browser is in columns

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import App from './pages/App/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="body">
+      <App />
+    </div>
   </React.StrictMode>
   
 );
